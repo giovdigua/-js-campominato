771644852cef8d34de72dd7b5caa77af3c252ec0
giovdigua <giovdigua@gmail.com>
2019-11-21 19:09:12 +0100

    Integration of level of difficulty

--- main.js
+++ main.js
@@ -1,37 +1,12 @@
-
-// 1) create an array of 16 mines with random number in range 1- 100
-var mines = generateMines(16,1,100);
-console.log(mines);
-
-// 2)Main logic games -- ask to the user a number in range 1 100 while dosen't hit a mines
-
-
+//Welcome and socpe game message
 alert('Welcome on minefield clone');
 alert('The game scope it\'s choose a number and dosen\'t hit a mine');
-//Variable that check if the user dosen'y hit a mine(He's live!!)
-var alive = true
+//User choose the difficulty level
+var difficultyNumber = chooseDifficulty(parseInt(prompt('Enter a number between 0 and 2 to choose the game\'s difficulty')));
+//Start the main game loop
+mainGame(difficultyNumber);
 
-do{
-    //Variable that contain the attempt of the user
-
-    var prof=0;
-    //check if try it's a number in valid range
-    //while (prof== NaN &&((prof<1)&&(prof>100))) {
-    do {
-        prof = prompt('Enter a number between 1 and 100');
-    } while (isNaN(prof) || ((prof<1)||(prof>100)));
-
-
-    if (hit(mines,prof)) {
-        alive = false;
-        alert('Game Over!You died!You stepped on a mine!');
-    }else{
-        alert('You\'re lucky! Continue the game.');
-
-    }
-
-}while (alive)
-
+/* -- Function section -- */
 //function for random number
 function generaRandom(min, max) {
     var random = Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -64,4 +39,49 @@ function hit(list,number){
         }
     }
     return hitIt;
+}
+
+//Main logic games -- ask to the user a number in range 1 100 while dosen't hit a mines
+
+function mainGame(level) {
+    //Create an array of 16 mines with random number in range 1- maxLevelDifficulty
+    var mines = generateMines(16,1,level);
+    //Variable that check if the user dosen't hit a mine(He's live!!)
+    var alive = true
+
+    do{
+        //Variable that contain the attempt of the user
+
+        var prof=0;
+        //check if try it's a number in valid range
+        //while (prof== NaN &&((prof<1)&&(prof>100))) {
+        do {
+            prof = prompt('Enter a number between 1 and ' + level);
+        } while (isNaN(prof) || ((prof<1)||(prof>level)));
+
+
+        if (hit(mines,prof)) {
+            alive = false;
+            alert('Game Over!You died!You stepped on a mine!');
+        }else{
+            alert('You\'re lucky! Continue the game.');
+
+        }
+
+    }while (alive)
+}
+//function that set the level of difficulty
+function chooseDifficulty(difficulty){
+    var maxLevelDifficulty;
+    if (difficulty == 0) {
+        maxLevelDifficulty = 100;
+    } else if (difficulty == 1) {
+        maxLevelDifficulty = 80;
+    }else if(difficulty == 2){
+        maxLevelDifficulty = 50;
+    }else{
+        //not correct choose level set Default
+        maxLevelDifficulty = 100;
+    }
+    return maxLevelDifficulty;
 }
