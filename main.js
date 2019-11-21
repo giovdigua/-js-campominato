e197572b7075878c4b89f43dafbdeb2c59d18505
giovdigua <giovdigua@gmail.com>
2019-11-21 16:43:49 +0100

    definiction of the function for random number and creation of mines

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+//function for random number
+function generaRandom(min, max) {
+    var random = Math.floor(Math.random() * (max - min + 1) ) + min;
+    return random;
+}
 
+//function for generate mines, choosing numbers of mine , min random number  and max random number.Default numbers of mines 5
+function generateMines(maxNumberOfMines = 5,minRandomNumber,maxRandomNumber){
+    var array = [];
+    var numberOfIteraction = 0;
+    while(array.length < maxNumberOfMines) {
+        var number = generaRandom(minRandomNumber, maxRandomNumber);
+
+
+        if(!array.includes(number)) {
+            array.push(number);
+        }
+        numberOfIteraction++;
+    }
+
+}
